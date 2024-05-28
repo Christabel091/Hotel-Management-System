b848debb54e216542c3c2dc218f9bf3405dc0c87
Christabel <obinwosugosiorah@gmail.com>
2024-05-27 22:21:02 -0500

    source files

---
+++ cpp/main.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<vector>
+//control the general flow of everything
+
+int main(){
+    std::cout << "Thank you for visiting your hotel";
+    std::cout << "\n What would you like to do today";
+}
