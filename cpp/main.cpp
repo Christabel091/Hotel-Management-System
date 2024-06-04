7da84b84ed6a6cb004a4d71aab2a04b6064f9d89
Christabel <obinwosugosiorah@gmail.com>
2024-06-03 21:58:50 -0500

    added some output statement

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -5,4 +5,7 @@
 int main(){
     std::cout << "Thank you for visiting your hotel";
     std::cout << "\n What would you like to do today";
+    std::cout << "press 1 to book a room\n 2 to make a reservation 3 cancel a reservation\n\t to release a room.\n\t to quit";
+    
+    return 0;
 }
