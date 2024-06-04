b768e82d1e6996e0b81a984fbc52b40ca69e22fd
Christabel <obinwosugosiorah@gmail.com>
2024-06-03 22:01:43 -0500

    made a little definition

---
+++ header/customer.h
@@ -0,0 +1,11 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+#include<iostream>
+class Customer{
+    std::string name;
+    int customer_id;
+    std::string email;
+    int phone_number;
+};
+
+#endif
