045f54f5fa26dd3abdef18a741376a7c43a9b9f7
CHRISTABEL <obinwosugosiorah@gmail.com>
2024-06-17 23:43:37 -0500

    modified

--- cpp/reservation.cpp
+++ cpp/reservation.cpp
@@ -4,6 +4,8 @@
 #include "reservation.h"
 #include<vector>
 #include "../header/room.h"
+#include<fstream>
+
 
 Reservation::Reservation(std::string userName, int userPhone){
     name = userName;
@@ -18,6 +20,17 @@ bool Reservation::makeReservations(int roomToReserve){
     }
 }
 
-std::vector Reservation::load_reservation(){
+std::vector<Reservation>  Reservation::load_reservation(){
+    std::ifstream infile;
+    infile.open("reservation.csv")
+    while (infile){
+        ??read from file
+    }
     //open file and load and fund rooms that are reserved on the date
+}
+
+bool Reservation::cancel_reservation(){
+    std::vector<Reservation> reserves;
+    reserves = load_reservation()
+
 }
