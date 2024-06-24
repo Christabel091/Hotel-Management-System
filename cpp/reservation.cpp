83f08eb23e3bb8c87596c26cb7e593de545b2216
CHRISTABEL <obinwosugosiorah@gmail.com>
2024-06-24 16:15:36 -0500

    implemenetd more of the class methods

--- cpp/reservation.cpp
+++ cpp/reservation.cpp
@@ -7,30 +7,88 @@
 #include<fstream>
 
 
-Reservation::Reservation(std::string userName, int userPhone){
+Reservation::Reservation(std::string userName, int room, int date, int days){
     name = userName;
-    number = userPhone;
+    room_number = room;
+    check_in_time = date;
+    time_period = days;
 }
 
-bool Reservation::makeReservations(int roomToReserve){
-    std::vector<Room> rooms;
-    rooms = room.loadRoom(rooms);
-    for (auto& room : rooms){
+
+bool Reservation::makeReservations(std::vector<Reservation> &reservations){
+    for (auto& reserve : reservations){
+        if (reserve.room_number == room_number){
+            if (reserve.check_in_time == check_in_time && reserve.isreserved == true){
+                isreserved = false;
+                return false;
+            }
+        }
+    }
+    isreserved = true;
+    return true;
+}
+
+std::vector<Reservation> loadReservations(const std::string& filename) {
+    std::vector<Reservation> reservations;
+    std::ifstream file(filename);
+    std::string line;
+    
+    // Skip the header line
+    std::getline(file, line);
+    
+    while (std::getline(file, line)) {
+        std::istringstream ss(line);
+        std::string token;
         
+        int reservationId;
+        std::string name;
+        long int number;
+        int room_number;
+        int check_in_time;
+        int time_period;
+        bool isreserved;
+        
+        std::getline(ss, token, ',');
+        reservationId = std::stoi(token);
+        std::getline(ss, name, ',');
+        std::getline(ss, token, ',');
+        number = std::stol(token);
+        std::getline(ss, token, ',');
+        room_number = std::stoi(token);
+        std::getline(ss, token, ',');
+        check_in_time = std::stoi(token);
+        std::getline(ss, token, ',');
+        time_period = std::stoi(token);
+        std::getline(ss, token, ',');
+        isreserved = (token == "true");
+        
+        reservations.push_back(Reservation(reservationId, name, number, room_number, check_in_time, time_period, isreserved));
     }
+    
+    return reservations;
 }
 
-std::vector<Reservation>  Reservation::load_reservation(){
-    std::ifstream infile;
-    infile.open("reservation.csv")
-    while (infile){
-        ??read from file
+bool Reservation::cancel_reservation(std::string Name, std::vector<Reservation> &reservations){
+    for (auto reserve : reservations){
+        if (reserve.name == Name){
+            std::string anwser;
+            std::cout << "You want to cancel reservation made by " << reserve.name << "to check in on " << reserve.check_in_time << "for " << reserve.time_period << " days?  (YES/NO)";
+            std::cin >> anwser;
+            std::toupper(anwser);
+            if ("YES"){
+                reserve.isreserved = false;
+                return true;
+            }
+            else{
+                std::cout << "Do you wish to cancel another another reservation or not cancel at all? ";
+                std::cout << "\n 'YES' to csncel anither reservation or 'NO' not to cancel any reservation ";
+                std::cout << anwser;
+                anwser = ::toupper(anwser);
+                if ("YES") continue;
+                else return false;
+            }
+        }
     }
-    //open file and load and fund rooms that are reserved on the date
-}
-
-bool Reservation::cancel_reservation(){
-    std::vector<Reservation> reserves;
-    reserves = load_reservation()
+    return false;
 
 }
